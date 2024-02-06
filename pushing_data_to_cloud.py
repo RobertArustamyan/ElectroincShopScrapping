e31d1f222621f17fece0a975b6d2dd47eec5ba76
Robert Arustamyan <robertarustamyan2@gmail.com>
2024-02-06 21:19:59 +0400

    Firebase data push

---
+++ pushing_data_to_cloud.py
@@ -0,0 +1,26 @@
+import os
+import json
+from firebase_admin import credentials, db, initialize_app
+
+cred = credentials.Certificate("electro-shops-arm-firebase-adminsdk-fznxq-e286074b55.json")
+initialize_app(cred, {
+    'databaseURL': 'https://electro-shops-arm-default-rtdb.europe-west1.firebasedatabase.app/'
+})
+
+def push_data_from_json(json_file_path, key):
+    with open(json_file_path, 'r') as f:
+        data = json.load(f)
+
+    ref = db.reference('/')
+    ref.child(key).set(data)
+    print(f"Data from '{json_file_path}' pushed to the database with key '{key}' successfully!")
+
+json_files_with_keys = [
+    {"path": "Data/3DPlanet.json", "key": "3d_planet_data"},
+    {"path": "Data/MobileCenterAllData.json", "key": "mobile_center_data"},
+    {"path": "Data/RedStoreAllData.json", "key": "red_store_data"},
+    {"path": "Data/ZigZagAllData.json", "key": "zig_zag_data"}
+]
+
+for file_info in json_files_with_keys:
+    push_data_from_json(file_info["path"], file_info["key"])
